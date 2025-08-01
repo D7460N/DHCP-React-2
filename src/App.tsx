9e0d5da8ca14cb32a00761c7627f71932d9201ef
D7460N <80736+dragontheory@users.noreply.github.com>
2025-08-01 12:01:25 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import './index.css';
+import { Layout } from './layout/Layout';
+import ScopeContent from './pages/Scopes';
+
+function App() {
+  return (
+    <Layout >
+      <ScopeContent />
+    </Layout>
+  );
+}
+
+export default App;
