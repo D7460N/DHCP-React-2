1749fe5ee62d35a391264f3eadfc5af25e7c1c72
D7460N <80736+dragontheory@users.noreply.github.com>
2025-08-01 14:11:27 -0400

    Create setupTests.ts

---
+++ src/setupTests.ts
@@ -0,0 +1,6 @@
+// jest-dom adds custom jest matchers for asserting on DOM nokes.
+// allows you to do thing like:
+// expect(element).toHaveTextContent(/reaact/i)
+// learn more: https://github.com/testing-library/jest-dom
+
+import '@testing-library/jest-dom';
