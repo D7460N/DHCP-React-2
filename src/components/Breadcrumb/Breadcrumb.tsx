839deee7058d8756b0fa70701391b817613e007a
D7460N <80736+dragontheory@users.noreply.github.com>
2025-08-06 16:24:14 -0400

    Create Breadcrumb.tsx

---
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,32 @@
+import { Link, useLocation } from 'react-router.dom';
+import { ChevronRight } from 'lucide-react';
+import { navigationConfig } from '../../config/navigation';
+import { findActiveNavigation } from '../../utils/navigation';
+
+export function Breadcurmb() {
+  const location = useLocation();
+  const { breadcrumb } = findActiveNavigation(navigationConfig, location.pathname);
+
+  if (breadcrumb.length <=1 ) return null;
+
+  return (
+    <nav className="flex" aria-label="Breadcrumb">
+      <ol className="flex items-center space-x-1">
+        {breadcrumb.map((item, index) => (
+          <li key={item.href} className="flex items-center">
+            {index === breadcrumb.length - 1 ? (
+              <span className="text-gray-700">{item.name}</span>
+            ) : (
+            <Link
+              to={item.href}
+              className="text-gray-600 hover:text-gray-900"
+              >
+              {item.name}
+            </Link>
+            )}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+)
