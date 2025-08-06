8362efb704c51572ac09d0323600837fd7ec367c
D7460N <80736+dragontheory@users.noreply.github.com>
2025-08-06 17:52:15 -0400

    Update Calendar.tsx

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -16,7 +16,7 @@ type Event = {
   time: string;
 };
 
-functin toDateKey(date: Date): string {
+function toDateKey(date: Date): string {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
