3664561beb19a0482b4d85df987767db79fea36a
D7460N <80736+dragontheory@users.noreply.github.com>
2025-08-07 18:51:00 -0400

    Create servers.ts

---
+++ src/api/servers.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const getServers = async () => {
+  try {
+    const response = await axios.get"https://localhost:44377/api/admin/Servers");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching serers:", error);
+    throw error;
+  }
+};
