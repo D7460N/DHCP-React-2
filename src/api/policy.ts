ce1c0e2b8a3a9c56a8e5762eb9672eb769451a2f
D7460N <80736+dragontheory@users.noreply.github.com>
2025-08-07 18:38:29 -0400

    Create policy.ts

---
+++ src/api/policy.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+export const createPolicy = async (data: any) => {
+  try {
+    const response = await axios.get("http://localhost:5110/api/policy", data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching policies", error);
+    throw error;
+  }
+};
+
+export const getPolicies = async (id: number) => {
+  try {
+    const response = await axios.get("http://localhost:5110/api/CloudProvider");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching policy by ID:", error);
+    throw error;
+  }
+};
+
+export const getPolicyById = async (id: number) => {
+  try {
+    const response = await axios.get(`http://localhost:5110/api/policy/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching policy by ID:", error);
+    throw error;
+  }
+};
+
+export const updatePolicy = async (id: number, updatePolicy: any) => {
+  try {
+    const response = await axios.put(`http://localhost:5110/api/policy/${id}`, updatePolicy);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating policy:", error);
+    throw error;
+  }
+};
+
+export const deletePolicy = async (id: number) => {
+  try {
+    const response = await axios.delete(`http://localhost:5110/api/policy/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting policy:", error);
+    throw error;
+  }
+};
