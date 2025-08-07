5038392f36d7828c0598da1721a2911b1c154ec4
D7460N <80736+dragontheory@users.noreply.github.com>
2025-08-07 18:51:43 -0400

    Update servers.ts

--- src/api/servers.ts
+++ src/api/servers.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getServers = async () => {
   try {
-    const response = await axios.get"https://localhost:44377/api/admin/Servers");
+    const response = await axios.get("https://localhost:44377/api/admin/Servers");
     return response.data;
   } catch (error) {
     console.error("Error fetching serers:", error);
